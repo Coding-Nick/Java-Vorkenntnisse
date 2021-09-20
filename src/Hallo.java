314d1d84ea35bd862de01fdf2f92926e09cb19f4
Nick <91061669+Coding-Nick@users.noreply.github.com>
2021-09-20 12:49:11 +0200

    SourceCode

---
+++ src/Hallo.java
@@ -0,0 +1,5 @@
+public class Hallo {
+    public static void main(String[] args) {
+        System.out.println(Willkommen.hallo); // <--- Hier greife ich auf den Prefix zu indem ich die Klasse Willkommen aufrufe und somit auch den Prefix dort drinnen!
+    }
+}
